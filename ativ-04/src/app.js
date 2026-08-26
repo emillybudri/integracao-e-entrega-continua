e6774b37f876b3daba9f2c7df91b12604e68f24e
Emilly Budri <emillybudribognar@gmail.com>
2026-08-26 16:42:34 -0300

    test(ativ-04): simula erro de linter com variavel nao utilizada

--- ativ-04/src/app.js
+++ ativ-04/src/app.js
@@ -4,6 +4,8 @@
 // Simulação do desafio: Erro de lint (descomentar para falhar linter)
 // const variavelSemUso = "erro_de_lint"
 
+const variavelSemUso = "erro_de_lint";
+
 function soma(a, b) {
   return a + b;
 }
